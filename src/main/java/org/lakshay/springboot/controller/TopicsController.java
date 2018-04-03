acab5214c69b937c524a7de31c374adcb1df432c
lakshay148 <lakshay148@gmail.com>
2018-04-03 17:05:58 +0530

    Method PUT/DELETE implemented

--- src/main/java/org/lakshay/springboot/controller/TopicsController.java
+++ src/main/java/org/lakshay/springboot/controller/TopicsController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.lakshay.springboot.model.Topic;
 import org.lakshay.springboot.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +20,25 @@ public class TopicsController {
 	@RequestMapping("/getTopics")
 	public List<Topic> getTopics(){
 		return topicService.getTopics();
+	}
+	
+	@RequestMapping("/getTopic/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/addTopic")
+	public void getTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/updateTopic/{id}")
+	public void updateTopic(@PathVariable String id,@RequestBody Topic topic) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(value="/deletTopic/{id}", method=RequestMethod.DELETE)
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
 	}
 }
--- src/main/java/org/lakshay/springboot/service/TopicService.java
+++ src/main/java/org/lakshay/springboot/service/TopicService.java
@@ -1,5 +1,6 @@
 package org.lakshay.springboot.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,12 +10,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(new Topic("1","Mathematics"),
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("1","Mathematics"),
 			new Topic("2","English"),
-			new Topic("3","Physics"));
+			new Topic("3","Physics")));
 	
 	public List<Topic> getTopics(){
 		return topics;
 	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
 
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic input) {
+		for(int i=0; i < topics.size(); i++) {
+			Topic topic = topics.get(i);
+			if(topic.getId().equals(id)) {
+				topics.set(i, input);
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t->t.getId().equals(id));
+	}
 }
